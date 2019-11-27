aab54953548357c8e44fe34f83c6c7a90310201b
whojes <whojes@gmail.com>
2019-11-27 17:03:28 +0900

    [customap] CkService/CkServiceExecutor annotation added to project.

--- poservicegroup/src/main/java/com/tmax/poapp/poservicegroup/service/HelloWorldGet.java
+++ poservicegroup/src/main/java/com/tmax/poapp/poservicegroup/service/HelloWorldGet.java
@@ -1,8 +1,13 @@
 package com.tmax.poapp.poservicegroup.service;
 
 import com.tmax.poapp.dataobject.poservicegroup.*;
+import com.tmax.proobject.common.CkService;
+import com.tmax.proobject.common.CkServiceExecutor;
+import com.tmax.proobject.common.HttpMethod;
 import com.tmax.proobject.model.service.ServiceObject;
 
+@CkService(serviceName = "hello-worlds", method = HttpMethod.GET)
+@CkServiceExecutor
 public class HelloWorldGet implements ServiceObject<HelloWorld, HelloWorld> {
     @Override
     public HelloWorld service(HelloWorld indto) {
